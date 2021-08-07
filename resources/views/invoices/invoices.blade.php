307b1df12d6341374b95b20ede6c3e504ac28f35
RedaMeziouni <meziounidb@gmail.com>
2021-08-07 19:00:16 +0100

    Table theme change

--- resources/views/invoices/invoices.blade.php
+++ resources/views/invoices/invoices.blade.php
@@ -13,7 +13,7 @@
 				<div class="breadcrumb-header justify-content-between">
 					<div class="my-auto">
 						<div class="d-flex">
-							<h4 class="content-title mb-0 my-auto">Invoices</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ Invoices List</span>
+							<h4 class="content-title mb-0 my-auto">Invoices</h4><span class="text-muted mt-1 tx-15 mr-2 mb-0">/ Invoices List</span>
 						</div>
 					</div>
 				</div>
@@ -39,37 +39,37 @@
 							</div>
 							<div class="card-body">
 								<div class="table-responsive">
-									<table id="example" class="table key-buttons text-md-nowrap">
+									<table id="example1" class="table key-buttons text-md-nowrap">
 										<thead>
 											<tr>
 												<th class="border-bottom-0">#</th>
 												<th class="border-bottom-0">Invoice Num</th>
-												<th class="border-bottom-0">Invoice Date</th>
-												<!-- <th class="border-bottom-0">Due Date</th> -->
+												<th class="border-bottom-0">Invoice</th>
+												<th class="border-bottom-0">Due Date</th>
 												<th class="border-bottom-0">Product</th>
 												<th class="border-bottom-0"> Departement</th>
 												<th class="border-bottom-0">Discount</th>
-												<th class="border-bottom-0">Tax</th>
-												<th class="border-bottom-0">Tax value</th>
-												<th class="border-bottom-0">Total</th>
+												 <th class="border-bottom-0">Tax</th> 
+												<th class="border-bottom-0">T. value</th>
+												<th class="border-bottom-0">Total</th> 
 												<th class="border-bottom-0">Status</th>
-												<!-- <th class="border-bottom-0">Description</th> -->
+												<th class="border-bottom-0">Description</th>
 											</tr>
 										</thead>
 										<tbody>
 											<tr>
 												<td>1</td>
+												<td>System </td>
+												<td>Edinburgh</td>
+												<td>61</td> 
+												<td>2011/04/25</td>
+												<td>$320</td>
+												<td>Tiger Nixon</td>
 												<td>System Architect</td>
 												<td>Edinburgh</td>
 												<td>61</td>
 												<td>2011/04/25</td>
-												<td>$320,800</td>
-												<!-- <td>Tiger Nixon</td> -->
-												<!-- <td>System Architect</td> -->
-												<td>Edinburgh</td>
-												<td>61</td>
-												<td>2011/04/25</td>
-												<td>$320,800</td>
+												<td>$320</td>
 											</tr>
 
 										</tbody>
