4c1eb0ba01f32afc170a6c5d582da2a113ff75b8
RedaMeziouni <meziounidb@gmail.com>
2021-08-08 18:37:07 +0100

    Supply Section Migration

---
+++ app/Http/Controllers/SupplyController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\supply;
+use Illuminate\Http\Request;
+
+class SupplyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\supply  $supply
+     * @return \Illuminate\Http\Response
+     */
+    public function show(supply $supply)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\supply  $supply
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(supply $supply)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\supply  $supply
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, supply $supply)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\supply  $supply
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(supply $supply)
+    {
+        //
+    }
+}
