1000856d603138b712a703c338febfd0c97e5291
RedaMeziouni <meziounidb@gmail.com>
2021-08-08 16:48:28 +0100

    Sections Update 101

--- app/Http/Controllers/SectionsController.php
+++ app/Http/Controllers/SectionsController.php
@@ -38,49 +38,29 @@ class SectionsController extends Controller
      */
     public function store(Request $request)
     {
-        $input = $request->all();
+        
+        $validatedData = $request->validate([
+            'section_name' => 'required|unique:sections|max:255',
+        ],[
 
-        // Cecking for the department name
-        $b_exists = sections::where('section_name','=',$input['section_name'])->exists();
+            'section_name.required' =>'! Please Enter a name ',
+            'section_name.unique' =>'! Name Already Exists ',
 
-        if($b_exists) {
-            session()->flash('Error', 'Already Exists');
-            return redirect('/sections');
-        } 
-        else {
+        
+        ]);
+        
             sections::create([
                 'section_name' => $request->section_name,
                 'description' => $request->description,
-                'Created_by' => (Auth::user()->name)
+                'Created_by' => (Auth::user()->name),
 
             ]);
             session()->flash('Add', 'Department has been Added Succefuly ');
             return redirect('/sections');
+        
         }
-    }
-
-        // $validatedData = $request->validate([
-        //     'section_name' => 'required|unique:sections|max:255',
-        // ],[
-
-        //     'section_name.required' =>'Please Enter a name !',
-        //     'section_name.unique' =>'Name Already Exists !',
-
-
-        // ]);
-
-        //     sections::create([
-        //         'section_name' => $request->section_name,
-        //         'description' => $request->description,
-        //         'Created_by' => (Auth::user()->name),
-
-        //     ]);
-        //     session()->flash('Add', 'Department has been Added Succefuly ');
-        //     return redirect('/sections');
-
-        // }
-
-    /**
+        
+        /**
      * Display the specified resource.
      *
      * @param  \App\sections  $sections
@@ -90,7 +70,7 @@ class SectionsController extends Controller
     {
         //
     }
-
+    
     /**
      * Show the form for editing the specified resource.
      *
@@ -101,7 +81,7 @@ class SectionsController extends Controller
     {
         //
     }
-
+    
     /**
      * Update the specified resource in storage.
      *
@@ -109,9 +89,31 @@ class SectionsController extends Controller
      * @param  \App\sections  $sections
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, sections $sections)
+    public function update(Request $request)
     {
         //
+        $id = $request->id;
+
+        $this->validate($request, [
+
+            'section_name' => 'required|max:255|unique:sections,section_name,'.$id,
+            'description' => 'required',
+        ],[
+
+            'section_name.required' =>'Enter departement Name',
+            'section_name.unique' =>'! Departement Name Already Exists',
+            'description.required' =>'Enter a Description',
+
+        ]);
+
+        $sections = sections::find($id);
+        $sections->update([
+            'section_name' => $request->section_name,
+            'description' => $request->description,
+        ]);
+
+        session()->flash('edit','Departement Edited Succesfuly');
+        return redirect('/sections');
     }
 
     /**
@@ -125,3 +127,38 @@ class SectionsController extends Controller
         //
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ### Store Data 1st Method ###
+//     $input = $request->all();
+
+//     // Cecking for the department name
+//     $b_exists = sections::where('section_name','=',$input['section_name'])->exists();
+
+//     if($b_exists) {
+//         session()->flash('Error', 'Already Exists');
+//         return redirect('/sections');
+//     } 
+//     else {
+//         sections::create([
+//             'section_name' => $request->section_name,
+//             'description' => $request->description,
+//             'Created_by' => (Auth::user()->name)
+
+//         ]);
+//         session()->flash('Add', 'Department has been Added Succefuly ');
+//         return redirect('/sections');
+//     }
+// }
--- resources/views/sections/sections.blade.php
+++ resources/views/sections/sections.blade.php
@@ -27,9 +27,18 @@ Sections
 				<!-- breadcrumb -->
 @endsection
 @section('content')
-				<!-- row -->
-				<div class="row">
-					<!-- Checkig befor Adding -->
+
+<!-- Checking for Error -->
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
 @if (session()->has('Add'))
 	<div class="alert alert-success alert-dismissible fade show" role="alert">
 		<strong>{{ session()->get('Add') }}</strong>
@@ -37,6 +46,15 @@ Sections
 		<span aria-hidden="true">&times;</span>
 		</button>
 	</div>
+@endif
+
+@if (session()->has('edit'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <strong>{{ session()->get('edit') }}</strong>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
 @endif
 
 <!-- Checking for error -->
@@ -48,6 +66,10 @@ Sections
 	</button>
 </div>
 @endif
+				<!-- row -->
+				<div class="row">
+					<!-- Checkig befor Adding -->
+
 					<!-- End Add Checking -->
 					<!-- Table -->
 					<div class="col-xl-12">
@@ -74,14 +96,26 @@ Sections
 											<!-- Number loop -->
 											<?php $i = 0; ?>
 											<!-- Get & Show data from DB -->
-											@foreach($sections as $s)
+											@foreach($sections as $x)
 											<!-- increment the $i -->
 											<?php $i++; ?>
 												<tr>
 													<td>{{ $i }}</td>
-													<td>{{ $s->section_name }}</td>
-													<td>{{ $s->description }}</td>
-													<td>{{ $s->section_name }}</td>
+													<td>{{ $x->section_name }}</td>
+													<td>{{ $x->description }}</td>
+													<td>
+													
+															<a class="modal-effect btn btn-sm btn-info" data-effect="effect-scale"
+																data-id="{{ $x->id }}" data-section_name="{{ $x->section_name }}"
+																data-description="{{ $x->description }}" data-toggle="modal"
+																href="#exampleModal2" title="تعديل"><i class="las la-pen"></i></a>
+												
+															<a class="modal-effect btn btn-sm btn-danger" data-effect="effect-scale"
+																data-id="{{ $x->id }}" data-section_name="{{ $x->section_name }}"
+																data-toggle="modal" href="#modaldemo9" title="حذف"><i
+																	class="las la-trash"></i></a>
+														
+                                    				</td>
 												</tr>
 											@endforeach
 
@@ -126,6 +160,65 @@ Sections
 			</div>
 		</div>
 					<!-- End pop up -->
+					    <!-- edit -->
+    <div class="modal fade" id="exampleModal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Edit Departement</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+
+                    <form action="sections/update" method="post" autocomplete="off">
+                        {{ method_field('patch') }}
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                            <input type="hidden" name="id" id="id" value="">
+                            <label for="recipient-name" class="col-form-label">: Department Name </label>
+                            <input class="form-control" name="section_name" id="section_name" type="text">
+                        </div>
+                        <div class="form-group">
+                            <label for="message-text" class="col-form-label">: Description</label>
+                            <textarea class="form-control" id="description" name="description"></textarea>
+                        </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary">Save</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Exit</button>
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- delete -->
+    <!-- <div class="modal" id="modaldemo9">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content modal-content-demo">
+                <div class="modal-header">
+                    <h6 class="modal-title">حذف القسم</h6><button aria-label="Close" class="close" data-dismiss="modal"
+                        type="button"><span aria-hidden="true">&times;</span></button>
+                </div>
+                <form action="sections/destroy" method="post">
+                    {{ method_field('delete') }}
+                    {{ csrf_field() }}
+                    <div class="modal-body">
+                        <p>هل انت متاكد من عملية الحذف ؟</p><br>
+                        <input type="hidden" name="id" id="id" value="">
+                        <input class="form-control" name="section_name" id="section_name" type="text" readonly>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">الغاء</button>
+                        <button type="submit" class="btn btn-danger">تاكيد</button>
+                    </div>
+            </div>
+            </form>
+        </div>
+    </div> -->
 				</div>
 				<!-- row closed -->
 			</div>
@@ -155,4 +248,19 @@ Sections
 <script src="{{URL::asset('assets/js/modal.js')}}"></script>
 <!--Internal  Datatable js -->
 <script src="{{URL::asset('assets/js/table-data.js')}}"></script>
+
+<!-- Edit Modal -->
+<script>
+    $('#exampleModal2').on('show.bs.modal', function(event) {
+        var button = $(event.relatedTarget)
+        var id = button.data('id')
+        var section_name = button.data('section_name')
+        var description = button.data('description')
+        var modal = $(this)
+        modal.find('.modal-body #id').val(id);
+        modal.find('.modal-body #section_name').val(section_name);
+        modal.find('.modal-body #description').val(description);
+    })
+
+</script>
 @endsection
