f57dcee7339a823c89c466d86c07deb452b79cf0
RedaMeziouni <meziounidb@gmail.com>
2021-08-09 05:45:50 +0100

    Invoices Details Table

--- database/migrations/2021_08_09_041947_create_invoices_details_table.php
+++ database/migrations/2021_08_09_041947_create_invoices_details_table.php
@@ -15,6 +15,16 @@ class CreateInvoicesDetailsTable extends Migration
     {
         Schema::create('invoices_details', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('id_Invoice');
+            $table->string('invoice_number', 50);
+            $table->foreign('id_Invoice')->references('id')->on('invoices')->onDelete('cascade');
+            $table->string('product', 50);
+            $table->string('Section', 999);
+            $table->string('Status', 50);
+            $table->integer('Value_Status');
+            $table->date('Payment_Date')->nullable();
+            $table->text('note')->nullable();
+            $table->string('user',300);
             $table->timestamps();
         });
     }
