fec139f08bf32b840b0d1e8750e259ec3db5775b
RedaMeziouni <meziounidb@gmail.com>
2021-08-12 19:35:54 +0100

    Dynamic Home Page Card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,7 +16,6 @@ Home
 					<div class="left-content">
 						<div>
 						  <h2 class="main-content-title tx-24 mg-b-1 mg-b-lg-1">Hi, welcome back!</h2>
-						  <p class="mg-b-0">Sales monitoring dashboard template.</p>
 						</div>
 					</div>
 					<div class="main-dashboard-header-right">
@@ -45,17 +44,19 @@ Home
 						<div class="card overflow-hidden sales-card bg-primary-gradient">
 							<div class="pl-3 pt-3 pr-3 pb-2 pt-0">
 								<div class="">
-									<h6 class="mb-3 tx-12 text-white">TODAY ORDERS</h6>
+									<h6 class="mb-3 tx-12 text-white"> TOTAL INVOICES </h6>
 								</div>
 								<div class="pb-0 mt-0">
 									<div class="d-flex">
 										<div class="">
-											<h4 class="tx-20 font-weight-bold mb-1 text-white">$5,74.12</h4>
-											<p class="mb-0 tx-12 text-white op-7">Compared to last week</p>
+											<h4 class="tx-20 font-weight-bold mb-1 text-white">
+												{{ number_format(\App\invoices::sum('Total'), 2) }} MAD
+											</h4>
+											<p class="mb-0 tx-12 text-white op-7"> T. Inv : {{ \App\invoices::count() }}</p>
 										</div>
 										<span class="float-right my-auto mr-auto">
 											<i class="fas fa-arrow-circle-up text-white"></i>
-											<span class="text-white op-7"> +427</span>
+											<span class="text-white op-7"> 100% </span>
 										</span>
 									</div>
 								</div>
@@ -67,17 +68,23 @@ Home
 						<div class="card overflow-hidden sales-card bg-danger-gradient">
 							<div class="pl-3 pt-3 pr-3 pb-2 pt-0">
 								<div class="">
-									<h6 class="mb-3 tx-12 text-white">TODAY EARNINGS</h6>
+									<h6 class="mb-3 tx-12 text-white"> UNPAD INVOICES </h6>
 								</div>
 								<div class="pb-0 mt-0">
 									<div class="d-flex">
 										<div class="">
-											<h4 class="tx-20 font-weight-bold mb-1 text-white">$1,230.17</h4>
-											<p class="mb-0 tx-12 text-white op-7">Compared to last week</p>
+											<h4 class="tx-20 font-weight-bold mb-1 text-white">
+												{{ number_format(\App\invoices::where('Value_Status', 2)->sum('Total'), 2) }} MAD
+											</h4>
+											<p class="mb-0 tx-12 text-white op-7">
+												UnP. Invoices : {{ \App\invoices::where('Value_Status', 2)->count() }}
+											</p>
 										</div>
 										<span class="float-right my-auto mr-auto">
 											<i class="fas fa-arrow-circle-down text-white"></i>
-											<span class="text-white op-7"> -23.09%</span>
+											<span class="text-white op-7"> 
+												% {{ number_format((\App\invoices::where('Value_Status', 2)->count() / \App\invoices::count() * 100), 2) }}
+											</span>
 										</span>
 									</div>
 								</div>
@@ -89,17 +96,23 @@ Home
 						<div class="card overflow-hidden sales-card bg-success-gradient">
 							<div class="pl-3 pt-3 pr-3 pb-2 pt-0">
 								<div class="">
-									<h6 class="mb-3 tx-12 text-white">TOTAL EARNINGS</h6>
+									<h6 class="mb-3 tx-12 text-white"> PAID INVOICES </h6>
 								</div>
 								<div class="pb-0 mt-0">
 									<div class="d-flex">
 										<div class="">
-											<h4 class="tx-20 font-weight-bold mb-1 text-white">$7,125.70</h4>
-											<p class="mb-0 tx-12 text-white op-7">Compared to last week</p>
+											<h4 class="tx-20 font-weight-bold mb-1 text-white">
+												{{ number_format(\App\invoices::where('Value_Status', 1)->sum('Total'), 2) }} MAD
+											</h4>
+											<p class="mb-0 tx-12 text-white op-7">
+												P. Invoices : {{ \App\invoices::where('Value_Status', 1)->count() }}
+											</p>
 										</div>
 										<span class="float-right my-auto mr-auto">
 											<i class="fas fa-arrow-circle-up text-white"></i>
-											<span class="text-white op-7"> 52.09%</span>
+											<span class="text-white op-7">
+												% {{ number_format((\App\invoices::where('Value_Status', 1)->count() / \App\invoices::count() * 100), 2) }}
+											</span>
 										</span>
 									</div>
 								</div>
@@ -111,17 +124,23 @@ Home
 						<div class="card overflow-hidden sales-card bg-warning-gradient">
 							<div class="pl-3 pt-3 pr-3 pb-2 pt-0">
 								<div class="">
-									<h6 class="mb-3 tx-12 text-white">PRODUCT SOLD</h6>
+									<h6 class="mb-3 tx-12 text-white"> PARTIAL INVOICES </h6>
 								</div>
 								<div class="pb-0 mt-0">
 									<div class="d-flex">
 										<div class="">
-											<h4 class="tx-20 font-weight-bold mb-1 text-white">$4,820.50</h4>
-											<p class="mb-0 tx-12 text-white op-7">Compared to last week</p>
+											<h4 class="tx-20 font-weight-bold mb-1 text-white">
+												{{ number_format(\App\invoices::where('Value_Status', 3)->sum('Total'), 2) }} MAD
+											</h4>
+											<p class="mb-0 tx-12 text-white op-7">
+												Ptl. Invoices : {{ \App\invoices::where('Value_Status', 3)->count() }}
+											</p>
 										</div>
 										<span class="float-right my-auto mr-auto">
 											<i class="fas fa-arrow-circle-down text-white"></i>
-											<span class="text-white op-7"> -152.3</span>
+											<span class="text-white op-7">
+												% {{ number_format((\App\invoices::where('Value_Status', 3)->count() / \App\invoices::count() * 100), 2) }}
+											</span>
 										</span>
 									</div>
 								</div>
